3db1833ba8f9de7dcb49c64b5cc959a2c7a8873c
Lokesh Patil <lokeshp6@kpit.com>
2025-12-10 11:11:49 +0530

    update file

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -5,22 +5,54 @@ resource "aws_vpc" "main" {
   tags = { Name = "de-project-vpc" }
 }
 
+# --- NEW: Internet Gateway (Internet ka Darwaza) ---
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "de-project-igw" }
+}
+
+# --- NEW: Route Table (Internet tak jaane ka raasta) ---
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = { Name = "de-public-rt" }
+}
+
 # Subnet 1 (Zone A)
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true # Public IP enable kiya
   tags = { Name = "de-subnet-1" }
 }
 
-# Subnet 2 (Zone B) - Ye Naya Hai
+# Subnet 2 (Zone B)
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = true # Public IP enable kiya
   tags = { Name = "de-subnet-2" }
 }
 
+# --- NEW: Route Table Associations (Subnets ko internet se jodna) ---
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Security Group
 resource "aws_security_group" "db_sg" {
   name        = "db_sg"
   description = "Allow inbound traffic"
@@ -30,7 +62,7 @@ resource "aws_security_group" "db_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"] 
+    cidr_blocks = ["0.0.0.0/0"] # NOTE: Production me ise restrict karein (Apna IP dalein)
   }
   
   ingress {
