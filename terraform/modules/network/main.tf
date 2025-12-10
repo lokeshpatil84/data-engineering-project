b9671be3edccee03c78b2b3abbb39c9da63fcfdd
Lokesh Patil <lokeshp6@kpit.com>
2025-12-10 10:33:53 +0530

    update file

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1"
+  availability_zone = "ap-south-1a"
   tags = { Name = "de-private-subnet" }
 }
 
--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,4 +1,4 @@
 output "vpc_id" { value = aws_vpc.main.id }
 output "private_subnet_id" { value = aws_subnet.private_subnet.id }
 output "security_group_id" { value = aws_security_group.db_sg.id }
-output "availability_zone" { value = "ap-south-1" }
+output "availability_zone" { value = "ap-south-1a" }
